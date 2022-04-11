631151b278d79e64db3c9d321ea7348c50f0861c
GonzaloIVT <gonzalo.vergara.t@gmail.com>
2022-04-11 01:37:36 -0400

    OK

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './components/Game';
+
+const App = () =>{
+  return (
+    <div className="container-fluid">
+      <h1>Tic Tac Toe in React.js</h1>
+      <Game />
+    </div>
+  )
+}
+
+export default App;
